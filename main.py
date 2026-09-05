6b1f64882ce3efbfc2697a589264d97444104fa0
navid191110 <navidfootball1391@gmail.com>
2026-09-05 14:30:57 +0330

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from rubka import Robot, filters
 
+TOKEN = "CDGFCJ0HZONMVANNGMOSWGKUEWFSLXFZRVKKJXSJXSWYRQEDHNZFJERHMLBCPVLC"
+
+bot = Robot(token=TOKEN)
+
+@bot.on_message(filters.is_command.start)
+async def start(bot, message):
+    await message.reply(
+        "⚽ سلام!\n\n"
+        "به ربات لیگ مجازی NVD خوش اومدی 🤩\n\n"
+        "ربات با موفقیت فعال شد."
+    )
+
+bot.run()
